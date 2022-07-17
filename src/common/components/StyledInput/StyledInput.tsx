70b271f2e396e443466b17c8d3a464e6f7fee715
Jedi <dalionfull@gmail.com>
2022-07-18 03:40:13 +0600

    add adaptive component

---
+++ src/common/components/StyledInput/StyledInput.tsx
@@ -0,0 +1,30 @@
+import React, {ChangeEvent, useState} from "react";
+import {TextField} from "@mui/material";
+
+
+type StyledInputPropsType = {
+    addPost: (value: string) => void
+}
+export const StyledInput: React.FC<StyledInputPropsType> = ({addPost}) => {
+    type ErrorType = string | '' | null
+    let [value, setValue] = useState<string>('')
+    let [error, setError] = useState<ErrorType>()
+    const onChangeEvent = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    const addPostCallback = () => {
+        if (value.trim() !== '') {
+            addPost(value)
+            setValue('')
+        } else if (value.trim() === '') {
+            setError('Invalid value')
+        }
+    }
+    const EnterKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Enter") {
+            addPostCallback()
+        }
+    }
+    return <TextField variant="outlined" label='NewPost' error={Boolean(error)} value={value} helperText={error}
+                      onChange={onChangeEvent} onKeyPress={(e) => EnterKeyPress(e)}/>
+}
