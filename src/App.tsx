1ebd4d74684c7c9650715c41ab387770c6399d1a
Jedi <dalionfull@gmail.com>
2022-07-18 22:53:35 +0600

    todo task

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ function App() {
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
     overflow: hidden;
     `
+    //TODO: move all styles in a separate file
     return (
         <GridContainer>
 
