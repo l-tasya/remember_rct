569162bc739948a84498a0086ee536551bad5acb
Jedi <dalionfull@gmail.com>
2022-07-19 22:34:12 +0600

    theme switch done

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,6 @@ function App() {
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.5);
     overflow: hidden;
     `
-    //TODO: move all styles in a separate file
     return (
         <GridContainer>
 
