5e46c7319ec4ba6e630ee6264b241e5452c05707
Jedi <dalionfull@gmail.com>
2022-07-20 17:57:14 +0600

    add nested routes

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import {Friends} from "./Friends/Friends";
+import {Groups} from "./Groups/Groups";
 import {Posts} from "./Posts/Posts";
 import {UserInfo} from "./User/UserInfo/UserInfo";
 
 export const Profile = () => {
+
     return (<div>
             <UserInfo />
-            <Posts />
+            <Routes>
+                <Route path="posts" element={<Posts />} />
+                <Route path="friends" element={<Friends />} />
+                <Route path="groups" element={<Groups />} />
+            </Routes>
         </div>
     )
 }
--- src/components/Profile/User/UserInfo/UserInfo.tsx
+++ src/components/Profile/User/UserInfo/UserInfo.tsx
@@ -3,47 +3,102 @@ import styled from "styled-components";
 import {useSelector} from "react-redux";
 import {AppStateType} from "../../../../redux/store/store";
 import {UserType} from "../../../../redux/reducers/profileReducer";
+import {StyledBlock} from "../../../../common/styles/styles";
+import {ThemeColorType} from "../../../../redux/reducers/settingsReducer";
+import {NavLink} from "react-router-dom";
 
 
 export const UserInfo: React.FC = () => {
-    const Container = styled.div`
-    
+    const Container = styled(StyledBlock)`
+      padding: 0;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr; 
+      grid-template-rows: repeat(6,1fr);
+      height: 400px;
     `
     const BackGroundEl = styled.div`
+        grid-column-start: 1;
+        grid-column-end: 4;
+        grid-row-start: 1;
+        grid-row-end: 4;
         display: flex;
         align-items: center;
         justify-content: center;
         width: 100%;
-        height: 250px;
-        background: ${(props: {background: string})=>props.background}
+        background: ${(props: { background: string }) => props.background}
     `
-    const UserAvatar = styled.img`
+    const UserAvatar = styled.div`
+        grid-column-start: 2;
+        grid-row-start: 2;
+        grid-row-end: 4;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+        img{
+        position: relative;
+        top: 5px;
+        display: flex;
+        justify-self: center;
         width: 152px;
         height: 152px;
         border-radius: 50%;
-        border: 10px solid white;
+        border: 10px solid white;}
+       
     `
+    const InfoContainer = styled.div`
+        grid-column-start: 2;
+        grid-row-start: 4;
+        grid-row-end: 6;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+`
     const Title = styled.div`
         font-size: 30px;
-        margin-bottom: 17px;
-        margin-top: 10px;
-        color: white;
+        color: gray;
         text-transform: capitalize;
         font-weight: 600;
     `
     const SubTitle = styled.div`
         color: lightgreen;
-        padding-bottom: 14px;
   
 
     `
+    const Footer = styled.div`
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 6;
+    grid-row-end: 7;
+    background: #b986fc;
+    display: grid;
+    margin: 0 5px;
+    grid-template-columns: repeat(10, 1fr);
+`
+    const FooterItem = styled(NavLink)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: white
+`
     let user: UserType = useSelector<AppStateType, UserType>(t => t.profile.userInfo)
+    let color = useSelector<AppStateType, ThemeColorType>(t => t.settings.themeColor)
     return (
         <Container>
-            <BackGroundEl background={'tomato'}/>
-            <UserAvatar src={user.photo}/>
-            <Title>{user.name} {user.surname}</Title>
-            <SubTitle>{user.eMail}</SubTitle>
+            <BackGroundEl background={color.first}/>
+            <UserAvatar >
+                <img src={user.photo} alt='profile photo undefined'/>
+            </UserAvatar>
+            <InfoContainer>
+                <Title>{user.name} {user.surname}</Title>
+                <SubTitle>{user.eMail}</SubTitle>
+            </InfoContainer>
+            <Footer>
+                <FooterItem to={'posts'}>posts</FooterItem>
+                <FooterItem to={'friends'}>friends</FooterItem>
+                <FooterItem to={'groups'}>groups</FooterItem>
+            </Footer>
 
         </Container>)
 }
