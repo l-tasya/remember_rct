e98d955038f662db38a99fee341268b673504353
Jedi <dalionfull@gmail.com>
2022-07-25 01:27:44 +0600

    added reducers for combine

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,16 @@
 import {combineReducers, createStore} from "redux"
+import { dialogsReducer } from "../reducers/dialogsReducer";
 import {profileReducer} from "../reducers/profileReducer"
 import {settingsReducer} from "../reducers/settingsReducer";
+import {messagesReducer} from "../reducers/messagesReducer";
 
 export type AppStateType = ReturnType<typeof reducers>
 
 const reducers = combineReducers({
     profile: profileReducer,
-    settings: settingsReducer
+    settings: settingsReducer,
+    dialogs: dialogsReducer,
+    messages: messagesReducer,
 })
 
 export const store = createStore(reducers)
