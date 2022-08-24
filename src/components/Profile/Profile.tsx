bddac2eacee47ec652b2fb910631d8444965969d
Jedi <dalionfull@gmail.com>
2022-08-24 15:38:05 +0600

    Post Container added('_')

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,8 +3,8 @@ import {Route, Routes} from "react-router-dom";
 import styled from "styled-components";
 import {Friends} from "./Friends/Friends";
 import {Groups} from "./Groups/Groups";
-import {Posts} from "./Posts/Posts";
 import {UserInfo} from "./User/UserInfo/UserInfo";
+import {PostsContainer} from "./Posts/PostsContainer";
 
 export const Profile = React.memo(() => {
     const Content = styled.div`
@@ -15,12 +15,12 @@ export const Profile = React.memo(() => {
             <UserInfo/>
             <Content>
                 <Routes>
-                    <Route path="posts" element={<Posts/>}/>
-                    <Route path="/" element={<Posts/>}/>
+                    <Route path="posts" element={<PostsContainer/>}/>
+                    <Route path="/" element={<PostsContainer/>}/>
                     <Route path="friends" element={<Friends/>}/>
                     <Route path="groups" element={<Groups/>}/>
                 </Routes>
             </Content>
         </div>
     )
-}
+})
