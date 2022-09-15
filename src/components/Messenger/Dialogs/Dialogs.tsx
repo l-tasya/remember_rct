f11c8a5aade55e520ef4abfcb5026cc1b228b396
Jedi <dalionfull@gmail.com>
2022-09-16 00:19:10 +0600

    added Users page to NavBar(9_6)

--- src/components/Messenger/Dialogs/Dialogs.tsx
+++ src/components/Messenger/Dialogs/Dialogs.tsx
@@ -14,31 +14,7 @@ export const Dialogs: React.FC<DialogPropsType> = React.memo(() => {
     let color = useSelector<AppStateType, ThemeColorType>(t => t.settings.themeColor)
     let dialogs = useSelector<AppStateType, DialogsType>(t => t.dialogs)
     const dispatch = useDispatch()
-    const Container = styled.div`
-    display: grid;
-    border-right: 0.3px solid rgb(211,211,211, 0.3);
-    align-items: center;
-    justify-items: center;
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(20, 50px);
-    overflow-y: scroll;
-    //firefox
-    scrollbar-color: ${color.first} white;
-    scrollbar-width: thin;
-    //google chrome
-    ::-webkit-scrollbar {
-    height: 10px;
-    width: 5px;
-    background: white;
-    }
-    ::-webkit-scrollbar-thumb {
-    background: ${color.first};
-}
-
-::-webkit-scrollbar-corner {
-    background: #000;
-}
-`
+    const Container = styled.div`\n    display: grid;\n    border-right: 0.3px solid rgb(211,211,211, 0.3);\n    align-items: center;\n    justify-items: center;\n    grid-template-columns: 1fr;\n    grid-template-rows: repeat(20, 50px);\n    overflow-y: scroll;\n    //firefox\n    scrollbar-color: ${color.first} white;\n    scrollbar-width: thin;\n    //google chrome\n    ::-webkit-scrollbar {\n    height: 10px;\n    width: 5px;\n    background: white;\n    }\n    ::-webkit-scrollbar-thumb {\n    background: ${color.first};\n}\n\n::-webkit-scrollbar-corner {\n    background: #000;\n}\n`
     const Title = styled.div`
       font-weight: 600;
       font-size: 1.5rem;
