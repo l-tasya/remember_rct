f4cb17a8657cd24cc2ac726e44b04dedd9a00192
Jedi <dalionfull@gmail.com>
2022-10-15 12:57:39 +0600

    fix

--- src/common/styles/styles.ts
+++ src/common/styles/styles.ts
@@ -1,49 +1,88 @@
 import styled from "styled-components";
-import {Paper} from "@mui/material";
-
-type WrapperPropsType = {
-    background: string
-}
 //global
-
+//scroll-------------------------
+export const Scroll = styled.div`
+    overflow-y: scroll;
+    position: absolute;
+    width: 100%;
+    //firefox
+    scrollbar-color: black white;
+    scrollbar-width: thin;
+    //google chrome
+    ::-webkit-scrollbar {
+    height: 10px;
+    width: 5px;
+    background: white;
+    }
+    ::-webkit-scrollbar-thumb {
+    background: black;
+}
+`
 //contentContainer wrap-------------------------
-export const PaddedContentContainer = styled.div`
+export const PaddedContentContainer = styled(Scroll)`
+    display: grid;
+    grid-template-columns: 1fr 4fr 1fr;
+    grid-template-rows: 1fr;
     grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 4;
-    margin-top: 16px;
     height: 100%;
+
+    
 `
 export const ContentContainerWithoutPadding = styled.div`
-   grid-column-start: 1;
-   grid-column-end: 4;
-   grid-row-start: 1;
+       width: 100%;
+       height: 100%;
+   
+
 `
-//--------------------------
 //paper--------------------------
-export const StyledBlock = styled(Paper)`
+type StyledBlockPropsType = {
+    radius?: number | 'none' | 'default'
+    padding?: string
+    elevation?: 1 | 3 | 'none' | 'default'
+    post?: boolean
+}
+const elevationReducer = (elevation: 3 | 1 | 'none' | 'default') => {
+    switch (elevation) {
+        case 'none': {
+            return 'box-shadow: 0px;'
+        }
+        case 'default': {
+            return 'box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12);'
+        }
+        case 1: {
+            return '  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12);'
+        }
+        case 3: {
+            return 'box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px;'
+        }
+        default: {
+            throw new Error('i dont understand this elevation(maybe it is invalid)')
+        }
+    }
+}
+const borderReducer = (value: number | 'none' | 'default') => {
+    switch (value) {
+        case 'none': {
+            return 'border-radius: 0px;'
+        }
+        case 'default': {
+            return `border-radius: 4px;`
+        }
+        default: {
+            return `border-radius: ${value};`
+        }
+    }
+}
+export const StyledBlock = styled.div`
     background: white;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-    ${(props: {padding?: string | 16}) =>(props.padding === 'none')?
-        `padding: 0px'4`
-    :
-    `padding: 16px`
-}
+    ${(props: StyledBlockPropsType) => props.radius ? borderReducer(props.radius) : borderReducer('default')}
+    ${(props: StyledBlockPropsType) => (props.padding === 'none') ? `padding: 0px;` : `padding: 16px;`}
+    ${(props: StyledBlockPropsType) => props.elevation ? elevationReducer(props.elevation) : elevationReducer('default')}
+    ${(props: StyledBlockPropsType) => props.post ? 'min-width: 200px; height: 200px; margin-top: 8px;' : ''}
 `
 
-
-
-
-
-
-//-------------------------
-export const AppWrapper = styled.div`
- background: #${(props: WrapperPropsType) => props.background};
- width: 100%;
- overflow-x: hidden;
-`
-
+//other-------------------------
 export const StyledIMGBadge = styled.div`
     background: #f5f5f5;
     display: flex;
