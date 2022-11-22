f7c80fe0003defecea5872f2232aab15d5c68bdb
Jedi <dalionfull@gmail.com>
2022-11-22 21:58:53 +0600

    now ProfileContainer will not do API requests

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -2,8 +2,7 @@ import React, {useCallback, useEffect} from "react";
 import {Profile} from "./Profile";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
-import {defaultUser, ProfileUserType, setProfileAC} from "../../redux/reducers/profileReducer";
-import axios from "axios";
+import {getProfileThunkCreator, ProfileUserType} from "../../redux/reducers/profileReducer";
 import {AppStateType} from "../../redux/store/store";
 
 
@@ -12,20 +11,10 @@ export const ProfileContainer = React.memo(() => {
     let dispatch = useDispatch()
 
     const user = useSelector<AppStateType, ProfileUserType>(t => t.profile.profile)
-    const setUserCallback = useCallback((user: ProfileUserType) => {
-        dispatch(setProfileAC(user))
-    },[dispatch])
-
+    const getProfile = useCallback((id: string | undefined) => getProfileThunkCreator(id)(dispatch), [dispatch])
     useEffect(() => {
-        if (param.userID === 'main') {
-            setUserCallback(defaultUser)
-        } else {
-            axios(`https://social-network.samuraijs.com/api/1.0/profile/${param.userID}`)
-                .then(response => {
-                    setUserCallback(response.data)
-                })
-        }
+        getProfile(param.userID)
 
-    }, [param.userID, user, setUserCallback])
+    }, [param.userID, getProfile])
     return <Profile user={user}/>
 })
