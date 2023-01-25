4701295d14bcf9fe844508a8d5632a88a9c96471
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-01-25 11:02:27 +0600

    added Settings nested routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import './App.scss';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import "./App.scss";
 import {Home} from "./components/Home/Home";
 import {Store} from "./components/Store/Store";
-import {Music} from './components/Music/Music';
+import {Music} from "./components/Music/Music";
 import {Messenger} from "./components/Messenger/Messenger";
-import {Settings} from "./components/Settings/Settings";
-import styled from 'styled-components';
-import {NavBar} from './components/NavBar/NavBar';
+import styled from "styled-components";
+import {NavBar} from "./components/NavBar/NavBar";
 import {ProfileContainer} from "./components/Profile/ProfileContainer";
-import {UsersContainer} from './components/Users/UsersContainer';
+import {UsersContainer} from "./components/Users/UsersContainer";
 import {HeaderContainer} from "./components/Header/HeaderContainer";
-import {Login} from './components/Login/Login';
+import {Login} from "./components/Login/Login";
+import {SettingsContainer} from "./components/Settings/SettingsContainer";
 
 
 const Container = styled.div`
@@ -36,7 +36,7 @@ const Content = styled.div`
 const App = React.memo(() => {
         return (
             <Container>
-                <HeaderContainer title={'TASYA NETWORK'}/>
+                <HeaderContainer title={"TASYA NETWORK"}/>
                 <NavBar/>
                 <Content>
                     <Routes>
@@ -46,7 +46,7 @@ const App = React.memo(() => {
                         <Route path={"remember_rct/Music"} element={<Music/>}/>
                         <Route path={"remember_rct/Store"} element={<Store/>}/>
                         <Route path={"remember_rct/Messenger/*"} element={<Messenger/>}/>
-                        <Route path={"remember_rct/Settings"} element={<Settings/>}/>
+                        <Route path={"remember_rct/Settings/*"} element={<SettingsContainer/>}/>
                         <Route path={"remember_rct/Login"} element={<Login/>}/>
                         <Route path={"remember_rct/Users"} element={<UsersContainer/>}/>
                     </Routes>
