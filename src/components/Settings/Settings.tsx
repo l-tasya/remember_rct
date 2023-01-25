dfe9e075c91669aa36f294e1ea4fcdbd5dff1165
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-01-25 11:01:31 +0600

    added Settings NavBar,made refactoring

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,95 +1,22 @@
-import Button from "@mui/material/Button/Button";
-import React, {useCallback, useContext, useMemo, useState} from "react";
-import styled from "styled-components";
-import {RadioButton} from "../../common/components/Radio/Radio";
-import {StyledBlock} from "../../common/styles/styles";
-import {useDispatch, useSelector} from "react-redux";
-import {AppStateType} from "../../redux/store/store";
-import {addThemeAC, setColorAC, ThemesArrayType} from "../../redux/reducers/settingsReducer";
-import JavascriptIcon from '@mui/icons-material/Javascript';
-import {AddTheme} from "./AddTheme";
-import {ColorModeContext} from "../../ToggleColor";
+import React from "react";
 import {PaddedContentContainer} from "../../common/styles/mui-styles";
-
-const Container = styled(StyledBlock)`
-          grid-column-start: 2;
-    
-`
-const Title = styled.div`
-    grid-column-start: 1;
-    grid-row-start: 1;
-    font-size: 17px;
-`
-const ColorRadio = styled.div`
-    grid-column-start: 1;
-    grid-column-end: 4;
-    display: grid;
-    grid-template-columns: repeat(25, 1fr);
-    grid-row-start: 2;
-    align-items: center;
-    margin-bottom: 5px;
-`
-const ApplyButton = styled(Button)`
-       grid-column-start: 3;
-       grid-row-start: 3;
-       width: 30px;
-       justify-self: flex-end;
-`
-const Icon = styled(JavascriptIcon)`
-    grid-row-start: 4;
-    grid-column-start: 3;
-    justify-self: flex-end;
-`
-const ColorPicker = styled.div`
-          display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    justify-content: center;
-    align-items: center;
-`
-export const Settings = React.memo(() => {
-        let themes = useSelector<AppStateType, ThemesArrayType>(t => t.settings.themes)
-        let initialActive = useSelector<AppStateType, string>(t => t.settings.active)
-        let [value, setValue] = useState<string>(initialActive)
-        let themeMode = useContext(ColorModeContext)
-        let color = useMemo(() => {
-            let curr = themes.find(t => t.id === value)
-            if (curr) {
-                return curr.first
-            }
-        }, [themes, value])
-
-        let dispatch = useDispatch()
+import {SettingsNavBar} from "./Nav/SettingsNavBar";
+import {Route, Routes} from "react-router-dom";
+import {ThemePickerContainer} from "./ThemePicker/ThemePickerContainer";
+import {StyledBlock} from "../../common/styles/styles";
 
 
-        const addTheme = useCallback((value: string) => {
-            dispatch(addThemeAC(value))
-        }, [dispatch])
-        const setColor = useCallback(() => {
-            dispatch(setColorAC(value))
-            themeMode.toggleColorMode(color || 'default')
-
-        }, [dispatch, value, color, themeMode])
+type SettingsPropsType = {}
+export const Settings: React.FC<SettingsPropsType> = React.memo(({}) => {
+        //TODO: Personal Info Component
         return <PaddedContentContainer>
-            <Container variety="post">
-                <ColorPicker>
-                    <Title>Select app theme</Title>
-                    <ColorRadio>
-                        {themes.map(t => <RadioButton
-                                key={t.id}
-                                title={t.first}
-                                c1={() => setValue(t.id)}
-                                checked={t.id === value}
-                            />
-                        )}
-                        <AddTheme addTheme={addTheme}/>
-                    </ColorRadio>
-                    <ApplyButton onClick={setColor} variant={'contained'}
-                                 size={'small'}>Apply</ApplyButton>
-                </ColorPicker>
-                <Icon sx={{fontSize: 30}}/>
-            </Container>
-
+            <SettingsNavBar/>
+            <StyledBlock id={"container"} variety="post">
+                <Routes>
+                    <Route path={"theme"} element={<ThemePickerContainer/>}/>
+                    <Route path={"personal-info"}/>
+                </Routes>
+            </StyledBlock>
         </PaddedContentContainer>
     }
 )
