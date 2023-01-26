e9128822cb01a35219886fea5dde758a6399cad2
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-01-26 20:54:01 +0600

    added Home route for Settings computer

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,21 +3,24 @@ import {PaddedContentContainer} from "../../common/styles/mui-styles";
 import {SettingsNavBar} from "./Nav/SettingsNavBar";
 import {Route, Routes} from "react-router-dom";
 import {ThemePickerContainer} from "./ThemePicker/ThemePickerContainer";
+import {EditProfileContainer} from "./EditProfile/EditProfileContainer";
+import styled from "styled-components";
 import {StyledBlock} from "../../common/styles/styles";
+import {Main} from "./SettingsMain/Main";
 
 
 type SettingsPropsType = {}
+const Content = styled(StyledBlock)``
 export const Settings: React.FC<SettingsPropsType> = React.memo(({}) => {
-
-    //TODO: Personal Info Component
-        return <PaddedContentContainer>
-            <SettingsNavBar/>
-            <StyledBlock id={"container"} variety="post">
-                <Routes>
-                    <Route path={"theme"} element={<ThemePickerContainer/>}/>
-                    <Route path={"personal-info"}/>
-                </Routes>
-            </StyledBlock>
-        </PaddedContentContainer>
+    return <PaddedContentContainer style={{height: "auto", gridTemplateColumns: "1fr 3fr 2fr"}}>
+        <SettingsNavBar/>
+        <Content id={"container"}>
+            <Routes>
+                <Route path={"/"} element={<Main/>}/>
+                <Route path={"/theme"} element={<ThemePickerContainer/>}/>
+                <Route path={"/personal-info"} element={<EditProfileContainer/>}/>
+            </Routes>
+        </Content>
+    </PaddedContentContainer>
     }
 )
