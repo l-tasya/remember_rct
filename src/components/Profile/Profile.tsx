52803250f8fee180db6fda50a1740710a04275fc
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-01-27 01:49:58 +0600

    deleted unnecessary

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import {Groups} from "./Groups/Groups";
-import {UserInfo} from "./User/UserInfo/UserInfo";
 import {PostsContainer} from "./Posts/PostsContainer";
-import {ProfileUserType} from "../../redux/reducers/profileReducer";
 import {Friends} from "./Friends/Friends";
 import {PaddedContentContainer} from "../../common/styles/mui-styles";
 import styled from "styled-components";
+import {ProfileInfoContainer} from "./ProfileInfo/ProfileInfoContainer";
 
-type ProfilePropsType = {
-    user: ProfileUserType
-}
+type ProfilePropsType = {}
 const Container = styled.div`
 margin-top: 5px;
 grid-column-start: 2;
@@ -19,11 +16,11 @@ const Wrapper = styled(PaddedContentContainer)`
 position: absolute;
 height: 100%;
 `
-export const Profile: React.FC<ProfilePropsType> = React.memo(({user}) => {
+export const Profile: React.FC<ProfilePropsType> = React.memo(() => {
     return (
         <Wrapper>
             <Container>
-                <UserInfo user={user}/>
+                <ProfileInfoContainer/>
                 <Routes>
                     <Route path="/" element={<PostsContainer/>}/>
                     <Route path="/posts" element={<PostsContainer/>}/>
--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,24 +1,29 @@
 import React, {useCallback, useEffect} from "react";
 import {Profile} from "./Profile";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {useParams} from "react-router-dom";
-import {getProfileThunkCreator, ProfileUserType} from "../../redux/reducers/profileReducer";
-import {AppStateType} from "../../redux/store/store";
-import {withAuthRedirect} from "../../common/hoc/WithAuthRedirect";
+import {getProfileThunkCreator, getStatusThunkCreator} from "../../redux/reducers/profileReducer";
 
 
 export const ProfileContainer = React.memo(() => {
     let param = useParams()
     let dispatch = useDispatch()
 
-    const user = useSelector<AppStateType, ProfileUserType>(t => t.profile.profile)
-    const getProfile = useCallback((id: string | undefined) => getProfileThunkCreator(id)(dispatch), [dispatch])
+    const getProfile = useCallback((id: number) => {
+        getProfileThunkCreator(id)(dispatch)
+    }, [dispatch])
+    const getStatus = useCallback((id: number) => {
+        getStatusThunkCreator(id)(dispatch)
+    }, [dispatch])
     useEffect(() => {
-        getProfile(param.userID)
+        if (param) {
+            let id = Number(param.userID)
+            getProfile(id)
+            getStatus(id)
+        }
         return () => {
         }
 
-    }, [param.userID, getProfile])
-    const Element = withAuthRedirect(Profile)
-    return <Element user={user}/>
+    }, [param.userID, getProfile, getStatus,])
+    return <Profile/>
 })
