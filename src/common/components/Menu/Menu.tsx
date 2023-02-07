3fa2f172d10d700fefb3e0a9538e325b79b900f6
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-07 17:04:08 +0600

    added Menu component(for header)

--- src/common/components/Menu/Menu.tsx
+++ src/common/components/Menu/Menu.tsx
@@ -1,89 +1,59 @@
-import React, {useEffect, useRef, useState} from 'react';
-import styled from 'styled-components';
-import {StyledIMGBadge} from '../../styles/mui-styles';
-import {StyledBlock} from '../../styles/styles';
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import styled from "@mui/material/styles/styled";
 
-type MenuPropsType = {
-    children: React.ReactNode
-    svg: React.ReactNode | null;
+interface IPropsItem {
+    svg: React.ReactNode | null
+    children: React.ReactNode[] | React.ReactNode
 }
-const MenuContent = styled(StyledBlock)`
-    position: absolute;
-    padding: 5px 0;
-    border: 1px solid #f1f3f4;
-    border-radius: 4px;
-    font-size: 14px;
-    top: 140%;
-    align-self: center;
-    box-shadow: rgba(0, 0, 0, 0.1) 0 10px 25px -5px, rgba(0, 0, 0, 0.04) 0px 13px 10px -5px;
-    transition:3s ease;
-   
-    
-`
-const MenuContainer = styled.div`
-    position: relative;
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-`
-const Badge = styled(StyledIMGBadge)`
-    width: 40px;
-    height: 40px;
-    margin: 0 10px;
-    border-radius: 50%;
-`
-export const Menu: React.FC<MenuPropsType> = ({children, svg}) => {
-    let [open, setOpen] = useState(false)
 
+const Badge = styled(IconButton)(({theme}) => ({
+    "svg": {
+        color: theme.palette.primary.main
+    },
+    "div": {}
+}))
 
-    //styles
+export const MenuBadge: React.FC<IPropsItem> = React.memo(({svg, children}) => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <React.Fragment>
 
-
-    //handlers
-    function useOutsideAlerter(ref: any) {
-        useEffect(() => {
-            /**
-             * Alert if clicked on outside of element
-             */
-            function handleClickOutside(event: any) {
-                if (ref.current && !ref.current.contains(event.target)) {
-                    setOpen(false)
+            <Badge
+                onClick={handleClick}
+                size="small"
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+            >
+                <Avatar sx={{
+                    width: "2.5rem",
+                    height: "2.5rem",
+                    background: (theme) => theme.palette.divider
+                }}>{svg}</Avatar>
+            </Badge>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                sx={{mt: 1.5}}
+                onClose={handleClose}
+                anchorOrigin={{horizontal: "center", vertical: "bottom"}}
+                transformOrigin={{horizontal: "center", vertical: "top"}}
+            >
+                {
+                    Array.isArray(children) ? children?.map((t: React.ReactNode, i) => <div
+                        onClick={handleClose} key={i}>{t}</div>) : children
                 }
-            }
-
-            // Bind the event listener
-            document.addEventListener('mousedown', handleClickOutside);
-            return () => {
-                // Unbind the event listener on clean up
-                document.removeEventListener('mousedown', handleClickOutside);
-            };
-        }, [ref]);
-    }
-
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef);
-    //
-    // let iconEl = () => {
-    //     switch (icon) {
-    //         case 'profile': {
-    //             return /><ProfileBadge src={profileIMG}
-    //         }
-    //         case 'notifications': {
-    //             return <NotificationsIcon/>
-    //         }
-    //         case 'store': {
-    //             return <LocalGroceryStoreIcon/>
-    //         }
-    //         case 'messages': {
-    //             return <ChatIcon/>
-    //         }
-    //         default: {
-    //             throw new Error(`dont understand icon${icon}`)
-    //         }
-    //     }
-    // }
-    return <MenuContainer ref={wrapperRef}>
-        <Badge onClick={() => setOpen(!open)}>{svg}</Badge>
-        {open ? <MenuContent padding={"none"} onClick={() => setOpen(false)}>{children}</MenuContent> : ""}
-    </MenuContainer>
-}
+            </Menu>
+        </React.Fragment>
+    );
+})
