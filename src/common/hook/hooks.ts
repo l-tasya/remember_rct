a24afc4aea624c6ad5e3a2fbc9f67b1b51a4d741
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-07 17:01:12 +0600

    added useAppSelector, useAppDispatch hooks

---
+++ src/common/hook/hooks.ts
@@ -0,0 +1,11 @@
+import {ThunkDispatch} from "redux-thunk";
+import {AnyAction} from "redux";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../redux/store/store";
+
+export type AppThunkDispatchType = ThunkDispatch<AppStateType, any, AnyAction>
+//APP DISPATCH
+export const useAppDispatch = () => useDispatch<AppThunkDispatchType>()
+
+
+export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
