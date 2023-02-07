b721bfb8bde339bd534686ff9e24d04984c63e71
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-07 17:30:13 +0600

    refactoring(adjusting for types)

--- src/components/Messenger/Messages/MessagesContainer.tsx
+++ src/components/Messenger/Messages/MessagesContainer.tsx
@@ -12,6 +12,7 @@ export const MessagesContainer: React.FC = () => {
     const addMessage = useCallback((dialogID: string, value: string, isMe: boolean) => {
         dispatch(addMessageAC(dialogID, value, isMe))
     }, [dispatch])
+
     let {pathname} = useLocation();
     const getID = (pathname: string): string => {
         return pathname.split('/')[3]
