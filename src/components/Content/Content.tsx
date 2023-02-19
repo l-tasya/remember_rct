52205034cc4602a3ddee5b8112e781733772e708
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 20:29:04 +0600

    bundle size

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
-import {Home} from "../Home/Home";
-import {ProfileContainer} from "../Profile/ProfileContainer";
-import {Music} from "../Music/Music";
-import {Store} from "../Store/Store";
-import {Messenger} from "../Messenger/Messenger";
-import {SettingsContainer} from "../Settings/SettingsContainer";
-import {Login} from "../Login/Login";
-import {UsersContainer} from "../Users/UsersContainer";
-import styled from "styled-components";
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Home} from '../Home/Home';
+import {ProfileContainer} from '../Profile/ProfileContainer';
+import {Music} from '../Music/Music';
+import {Store} from '../Store/Store';
+import {Messenger} from '../Messenger/Messenger';
+import {Login} from '../Login/Login';
+import {UsersContainer} from '../Users/UsersContainer';
+import styled from 'styled-components';
+import {Settings} from '../Settings/Settings';
 
 const Wrapper = styled.div`
         grid-row-start: 2;
@@ -22,15 +22,15 @@ const Wrapper = styled.div`
 export const Content: React.FC = React.memo(() => {
         return <Wrapper>
             <Routes>
-                <Route path={"remember_rct/"} element={<Home/>}/>
-                <Route path={"remember_rct/home"} element={<Home/>}/>
-                <Route path={"remember_rct/:userID/*"} element={<ProfileContainer/>}/>
-                <Route path={"remember_rct/Music"} element={<Music/>}/>
-                <Route path={"remember_rct/Store"} element={<Store/>}/>
-                <Route path={"remember_rct/Messenger/*"} element={<Messenger/>}/>
-                <Route path={"remember_rct/Settings/*"} element={<SettingsContainer/>}/>
-                <Route path={"remember_rct/Login"} element={<Login/>}/>
-                <Route path={"remember_rct/Users"} element={<UsersContainer/>}/>
+                <Route path={'remember_rct/'} element={<Home/>}/>
+                <Route path={'remember_rct/home'} element={<Home/>}/>
+                <Route path={'remember_rct/:userID/*'} element={<ProfileContainer/>}/>
+                <Route path={'remember_rct/Music'} element={<Music/>}/>
+                <Route path={'remember_rct/Store'} element={<Store/>}/>
+                <Route path={'remember_rct/Messenger/*'} element={<Messenger/>}/>
+                <Route path={'remember_rct/Settings/*'} element={<Settings/>}/>
+                <Route path={'remember_rct/Login'} element={<Login/>}/>
+                <Route path={'remember_rct/Users'} element={<UsersContainer/>}/>
             </Routes>
         </Wrapper>
     }
