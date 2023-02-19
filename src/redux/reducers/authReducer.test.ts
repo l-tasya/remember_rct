41e176062c33a94af89596879927977a78571236
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 20:08:51 +0600

    adjusted values to the actual types

--- src/redux/reducers/authReducer.test.ts
+++ src/redux/reducers/authReducer.test.ts
@@ -1,20 +1,23 @@
-import {authReducer, AuthStateType, setUserDataAC} from "./authReducer";
+import {AuthReducerType} from '../../common/types/types';
+import {authReducer, setUserDataAC} from './authReducer';
 
 test('auth reducer should set user data', () => {
-    const startState: AuthStateType = {
+    const startState: AuthReducerType = {
         id: 0,
         email: undefined,
         login: undefined,
         isAuth: false,
+        profile: null
     }
     let data = {
         id: 333,
         email: 'dalionfull@gmail.com',
-        login: 'tasya'
+        login: 'tasya',
+        isAuth: true,
     }
     const endState = authReducer(startState, setUserDataAC(data))
     expect(endState.email).toBe(data.email)
     expect(endState.login).toBe(data.login)
     expect(endState.id).toBe(data.id)
-    expect(endState.isAuth).toBe(true)
+    expect(endState.isAuth).toBe(data.isAuth)
 })
