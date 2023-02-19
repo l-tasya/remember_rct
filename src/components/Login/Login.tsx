c6d1755af657a901d29ee3af0d7ed496371bd5fc
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 20:30:21 +0600

    added formik

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,17 @@
-import React from "react";
-import styled from "styled-components";
-import {ContentContainerWithoutPadding} from "../../common/styles/mui-styles";
-import {StyledBlock} from "../../common/styles/styles";
-import {FormikErrors, useFormik} from "formik";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import React, {useCallback} from 'react';
+import styled from 'styled-components';
+import {StyledBlock} from '../../common/styles/styles';
 
-const Container = styled(ContentContainerWithoutPadding)`
+import {useAppDispatch, useAppSelector} from '../../common/hook/hooks';
+import {logIn} from '../../redux/reducers/authReducer';
+import {LoginForm} from './LoginForm/LoginForm';
+import {useNavigate} from 'react-router-dom';
 
-`
-const Block = styled(StyledBlock)`
+const Container = styled(StyledBlock)`
   margin: 16px;
   min-width: 200px;
   width: 20%;
-  height: 50%;
   text-align: center;
-  //_____
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -29,75 +25,34 @@ const Title = styled.div`
   font-weight: 600;
 `
 export const Login: React.FC = React.memo(() => {
-    return <Container>
-        <Block elevation={3} radius={13}>
-            <Title>Login</Title>
-            <LoginForm/>
-        </Block>
+    const dispatch = useAppDispatch();
+    const loginCallback = useCallback((email: string, password: string, rememberMe: boolean) => {
+        dispatch(logIn(email, password, rememberMe))
+    }, [dispatch])
+    const navigate = useNavigate()
+    const auth = useAppSelector(t => t.auth)
+    if (auth.isAuth) {
+        navigate(`/remember_rct/${auth.id}/posts`)
+    }
+    return <Container elevation={3} radius={13}>
+        <Title>Login</Title>
+        <LoginForm login={loginCallback}/>
     </Container>
 
 })
 
 
-interface FormValues {
-    email: string;
-    password: string;
-}
 
-const LoginForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        validate: (values) => {
-            const errors: FormikErrors<FormValues> = {};
-            if (!values.email) {
-                errors.email = "Required";
-            } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-                errors.email = "Invalid email address";
-            }
-            return errors;
-        },
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
 
-    return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <TextField
-                    fullWidth
-                    id="email"
-                    name="email"
-                    label="Email"
-                    size="small"
-                    variant="standard"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                />
-                <TextField
-                    fullWidth
-                    id="password"
-                    name="password"
-                    label="Password"
-                    size="small"
-                    variant="standard"
-                    type="password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
-                />
-                <Button color="primary" variant="contained" fullWidth type="submit">
-                    Submit
-                </Button>
-            </form>
-        </div>
-    );
-};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {FormikErrors, useFormik} from 'formik';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import styled from 'styled-components';
+import Button from '@mui/material/Button/Button';
+
+export const Wrapper = styled.form`
+
+`
+
+interface FormValues {
+    email: string;
+    password: string;
+    rememberMe: boolean;
+}
+
+interface IProps {
+    login: (email: string, login: string, rememberMe: boolean) => void
+}
+
+
+const LoginFormValidation = (values: FormValues) => {
+    const errors: FormikErrors<FormValues> = {};
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+        errors.email = 'Invalid email address';
+    }
+    return errors;
+}
+export const LoginForm: React.FC<IProps> = ({login}) => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: LoginFormValidation,
+        onSubmit: (values) => {
+            setTimeout(() => {
+                login(values.email, values.password, values.rememberMe)
+                formik.setSubmitting(false)
+            }, 400);
+
+
+        }
+    });
+
+    return (
+        <Wrapper onSubmit={formik.handleSubmit}>
+            <TextField
+                fullWidth
+                name="email"
+                label="Email"
+                size="small"
+                variant="standard"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+                fullWidth
+                name="password"
+                label="Password"
+                size="small"
+                variant="standard"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+            />
+            <div>Remember<Checkbox
+                name="rememberMe"
+                size="small"
+                value={formik.values.email}
+                checked={formik.values.rememberMe}
+                onChange={formik.handleChange}
+            /></div>
+            <Button color="primary" variant="contained" fullWidth type="submit" disabled={formik.isSubmitting}>
+                Submit
+            </Button>
+        </Wrapper>
+    );
+};
