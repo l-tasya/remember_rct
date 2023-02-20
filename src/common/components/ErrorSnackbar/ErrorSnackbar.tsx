ddb4859b87caf226deb8346d36f983ea97a3fae7
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-20 14:53:02 +0600

    added ErrorSnackbar component for show errors

---
+++ src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Snackbar from '@mui/material/Snackbar/Snackbar';
+import {useAppDispatch, useAppSelector} from '../../hook/hooks';
+import {setErrorAC, setLoadingStatusAC} from '../../../redux/reducers/appReducer';
+import MuiAlert, {AlertProps} from '@mui/material/Alert/Alert';
+import {Stack} from '@mui/material';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const ErrorSnackbar: React.FC = () => {
+    const dispatch = useAppDispatch()
+    const error = useAppSelector(t => t.app.error)
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            dispatch(setErrorAC(null));
+        }
+        dispatch(setErrorAC(null));
+        dispatch(setLoadingStatusAC('idle'))
+    };
+    return <Stack spacing={2} sx={{width: '100%'}}><Snackbar open={Boolean(error)} autoHideDuration={3000}
+                                                             onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
+            {error}
+        </Alert>
+    </Snackbar>
+    </Stack>
+}
+
+
+
+
