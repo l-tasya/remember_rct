7288aba8e21d7ca5ce55e07f1ad7c865115a34c9
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-20 15:02:35 +0600

    optimized bundle size

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -9,6 +9,11 @@ import {Login} from '../Login/Login';
 import {UsersContainer} from '../Users/UsersContainer';
 import styled from 'styled-components';
 import {Settings} from '../Settings/Settings';
+import {LinearProgress} from '@mui/material';
+import {stylesForLoading} from '../../App';
+import {useAppSelector} from '../../common/hook/hooks';
+import {ErrorSnackbar} from '../../common/components/ErrorSnackbar/ErrorSnackbar';
+import {AlertSnackbar} from '../../common/components/AlertSnackbar/AlertSnackbar';
 
 const Wrapper = styled.div`
         grid-row-start: 2;
@@ -20,7 +25,11 @@ const Wrapper = styled.div`
         position: relative;
     `
 export const Content: React.FC = React.memo(() => {
+    const status = useAppSelector(t => t.app.status)
         return <Wrapper>
+            {status === 'loading' && <LinearProgress sx={stylesForLoading}/>}
+            {status === 'failed' &&
+            <LinearProgress variant={'determinate'} value={100} color={'error'} sx={stylesForLoading}/>}
             <Routes>
                 <Route path={'remember_rct/'} element={<Home/>}/>
                 <Route path={'remember_rct/home'} element={<Home/>}/>
@@ -32,6 +41,8 @@ export const Content: React.FC = React.memo(() => {
                 <Route path={'remember_rct/Login'} element={<Login/>}/>
                 <Route path={'remember_rct/Users'} element={<UsersContainer/>}/>
             </Routes>
+            <ErrorSnackbar/>
+            <AlertSnackbar/>
         </Wrapper>
     }
 )
