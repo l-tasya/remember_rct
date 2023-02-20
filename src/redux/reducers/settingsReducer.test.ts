2201c47de415309dbbc7aed038b397fc6d92634a
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-20 15:20:47 +0600

    adjusted values for actual types

--- src/redux/reducers/settingsReducer.test.ts
+++ src/redux/reducers/settingsReducer.test.ts
@@ -1,32 +1,22 @@
-import {addThemeAC, setColorAC, settingsReducer, SettingsStateType} from "./settingsReducer"
-import {v1} from "uuid";
+import {addThemeAC, setColorAC, settingsReducer, SettingsStateType} from './settingsReducer'
+import {v1} from 'uuid';
 
+const startState: SettingsStateType = {
+    active: '',
+    themes: [
+        {id: v1(), first: '#8d59ac',},
+        {id: v1(), first: 'mediumspringgreen',},
+        {id: v1(), first: '#1a74ed',},
+        {id: v1(), first: '#dc2121',},
+        {id: v1(), first: '#ff0084',},
+    ]
+}
 test('settingReducer should change theme color', () => {
-    const startState: SettingsStateType = {
-        active: '',
-        themes:  [
-            {id: v1(), first: '#8d59ac',},
-            {id: v1(), first: 'mediumspringgreen',},
-            {id: v1(), first: '#1a74ed',},
-            {id: v1(), first: '#dc2121',},
-            {id: v1(), first: '#ff0084',},
-        ]
-    }
     const id = startState.themes[3].id
     const endState = settingsReducer(startState, setColorAC(id))
     expect(endState.active).toBe(id)
 })
 test('settingReducer should add theme color', () => {
-    const startState: SettingsStateType = {
-        active: '',
-        themes: [
-            {id: v1(), first: '#8d59ac',},
-            {id: v1(), first: 'mediumspringgreen',},
-            {id: v1(), first: '#1a74ed',},
-            {id: v1(), first: '#dc2121',},
-            {id: v1(), first: '#ff0084',},
-        ]
-    }
     let newValue = '#a34848'
 
     const endState = settingsReducer(startState, addThemeAC(newValue))
